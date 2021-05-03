03e28bdad2662c4346db0f2dfcc2b62c13d891ca
Elisa Shapiro <elisashapiro@gmail.com>
2021-05-03 00:32:09 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -7,7 +7,7 @@ function teenager(age) {
   if (age >= 13 && age <= 19) {
   return "You are a teenager!";
 } else {
-  return "You are not a teenager!";}
+  return "You are not a teenager";}
 }
 
 function ageChecker(age) {
@@ -22,15 +22,33 @@ function ageChecker(age) {
 function ternaryTeenager(age) {
   if (age >=13 && age <=19) {
   return "You are a teenager";
-} else {return "You are not a teenager";}
+} else {return "You are not a teenager!";}
 }
 
 function switchAge(age) {
 switch(age) {
-  case (age >= 13 && age <= 19):
+  case (13):
   return ("You are a teenager");
-  break;
+
+  case (14):
+  return ("You are a teenager");
+
+  case (15):
+  return ("You are a teenager");
+
+  case (16):
+  return ("You are a teenager");
+
+  case (17):
+  return ("You are a teenager");
+
+  case (18):
+  return ("You are a teenager");
+
+  case (19):
+  return ("You are a teenager");
+
   default:
-  return ('You have an age')
-  break; }
+  return ("You have an age");
+}
 }
